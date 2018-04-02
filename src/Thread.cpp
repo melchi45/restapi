ee45d602b0c7d28f89b092828f83d0934199e1e9
Youngho Kim <melchi45@gmail.com>
2018-04-02 16:54:19 +0900

    add apache copyright
    add apache copyright

--- src/Thread.cpp
+++ src/Thread.cpp
@@ -1,47 +1,36 @@
 /*******************************************************************************
- *  Copyright (c) 2016 Hanwha Techwin Co., Ltd.
- *
- *  Licensed to the Hanwha Techwin Software Foundation under one or more
- *  contributor license agreements.  See the NOTICE file distributed with
- *  this work for additional information regarding copyright ownership.
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
- *  Smart Home Camera - Hanwha B2C Action Cam Project
- *  http://www.samsungsmartcam.com
- *
- *  Security Solution Division / Camera S/W Development Team
- *  Home Security Camera SW Dev. Group
- *
- *  @file
- *  Contains implementation of logging tools.
- *
- *  $Author$
- *  $LastChangedBy$
- *  $Date$
- *  $Revision$
- *  $Id$
- *  $HeadURL$
- *******************************************************************************
-
-  MODULE NAME:
-
-  REVISION HISTORY:
-
-  Date        Ver Name                    Description
-  ----------  --- --------------------- -----------------------------------------
-  07-Jun-2016 0.1 Youngho Kim             Created
- ...............................................................................
-
-  DESCRIPTION:
-
-
- ...............................................................................
- *******************************************************************************/
+*  Copyright (c) 1998 MFC Forum
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+* Module Name:
+*
+* Revision History:
+*
+* Date        Ver Name                    Description
+* ----------  --- --------------------- -----------------------------------------
+* 07-Jun-2016 0.1 Youngho Kim             Created
+* ----------  --- --------------------- -----------------------------------------
+*
+* DESCRIPTION:
+*
+*  $Author:
+*  $LastChangedBy:
+*  $Date:
+*  $Revision: 2949 $
+*  $Id:
+*  $HeadURL:
+*******************************************************************************/
 #include <signal.h>
 //#include <errno.h>
 #include <string.h>
--- src/log_utils.c
+++ src/log_utils.c
@@ -1,48 +1,36 @@
 /*******************************************************************************
- *  Copyright (c) 2016 Hanwha Techwin Co., Ltd.
- *
- *  Licensed to the Hanwha Techwin Software Foundation under one or more
- *  contributor license agreements.  See the NOTICE file distributed with
- *  this work for additional information regarding copyright ownership.
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
- *  Smart Home Camera - Hanwha B2C Action Cam Project
- *  http://www.samsungsmartcam.com
- *
- *  Security Solution Division / Smart Home Camera SW Dev. Group
- *  B2C Action Camera SW Dev. Group
- *
- *  @file
- *  Contains implementation of logging tools.
- *
- *  $Author: young_ho_kim $
- *  $LastChangedBy: young_ho_kim $
- *  $Date: 2016-08-19 16:44:07 +0900 (Fri, 19 Aug 2016) $
- *  $Revision: 2352 $
- *  $Id: log_utils.c 2352 2016-08-19 07:44:07Z young_ho_kim $
- *  $HeadURL: http://ctf1.stw.net/svn/repos/wearable_camera/trunk/Source/Pixcam/Wear-1.0.5/app/dm/log/log_utils.c $
- *******************************************************************************
-
-  MODULE NAME:
-
-  REVISION HISTORY:
-
-  Date        Ver Name                    Description
-  ----------  --- --------------------- -----------------------------------------
-  04-Jun-2016 0.1 Youngho Kim             Created
- ...............................................................................
-
-  DESCRIPTION:
-
-
- ...............................................................................
- *******************************************************************************/
-
+*  Copyright (c) 1998 MFC Forum
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+* Module Name:
+*
+* Revision History:
+*
+* Date        Ver Name                    Description
+* ----------  --- --------------------- -----------------------------------------
+* 07-Jun-2016 0.1 Youngho Kim             Created
+* ----------  --- --------------------- -----------------------------------------
+*
+* DESCRIPTION:
+*
+*  $Author:
+*  $LastChangedBy:
+*  $Date:
+*  $Revision: 2949 $
+*  $Id:
+*  $HeadURL:
+*******************************************************************************/
 #include <stdarg.h>
 #include <stdlib.h>
 #include <stdio.h>
